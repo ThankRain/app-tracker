5eb987bf93e00166b2b518356c04c867252e3562
thankrain <1573856599@qq.com>
2022-08-24 19:10:53 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use] extern crate rocket;
+
+use rocket::{Build, Rocket};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index])
+}
